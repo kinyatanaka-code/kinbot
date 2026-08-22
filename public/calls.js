f57f69158daa667bff4fe96530a677e6af8ab22c
kinyatanaka-code <kinya.tanaka@neo-career.co.jp>
2026-08-22 12:40:28 +0900

    CSV: public/calls.js クロス優先・種別表示

--- public/calls.js
+++ public/calls.js
@@ -1394,8 +1394,9 @@ async function csvSend(dryRun) {
 
       // 途中経過も出しておく
       if (out) {
-        out.innerHTML = '<table class="sh-table"><tr><th>会社名</th><th>担当者</th><th>架電日</th><th>最終ステータス</th><th>コメント</th><th>状態</th></tr>' +
+        out.innerHTML = '<table class="sh-table"><tr><th>会社名</th><th>担当者</th><th>リード</th><th>架電日</th><th>最終ステータス</th><th>コメント</th><th>状態</th></tr>' +
           meisai.slice(0, 300).map((x) => `<tr><td>${esc(x.company || "")}</td><td>${esc(x.person || "")}</td>` +
+            `<td>${esc(x["リード種別"] || "-")}</td>` +
             `<td>${esc(x["架電日"] || "")}</td>` +
             `<td>${esc(x["ステータス"] || "-")}</td>` +
             `<td class="kc-cmt">${esc(x["コメント"] || "")}</td>` +
