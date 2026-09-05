ef196e676915e5f9aa9fa37e12d2d9b554c68d16
kinyatanaka-code <kinya.tanaka@neo-career.co.jp>
2026-09-05 14:24:37 +0900

    左サイドメニューをデスクトップで固定(sticky)。本文だけスクロール。BUILD_TAG=2026-09-05b

--- public/nav.js
+++ public/nav.js
@@ -34,7 +34,7 @@ window.addEventListener("error", (e) => {
 (function () {
   if (!document.querySelector('script[src$="kbchat.js"]')) {
     const sc = document.createElement("script");
-    sc.src = "kbchat.js?v=20260905a";
+    sc.src = "kbchat.js?v=20260905b";
     sc.defer = true;
     document.head.appendChild(sc);
   }
